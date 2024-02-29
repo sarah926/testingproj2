7ee7738f7490956a752d6e76810a7ebdafef09e7
stoll <stoll@uoguelph.ca>
2024-02-29 13:50:50 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -9,6 +9,9 @@ import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
+  Image,
+  ImageBackground,
+  FlatList, 
   ScrollView,
   StatusBar,
   StyleSheet,
@@ -24,36 +27,30 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import * as data from './recipe_list.json';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+type ItemProps = {title: string, image: string, speed: string, price: string, level: string};
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const Item = ({title, image, speed, price, level}: ItemProps) => (
+  <View style={styles.item}>
+      <ImageBackground style={{width:"100%", height: "100%", borderRadius: 10,
+}} source={{uri: image}} resizeMode='cover'>
+        <View style={styles.row}>
+          <Text style={styles.subtitle}>{data.recipes[0].recipeAuthor}</Text>
+          <Image style={{width: 20, height: 20, marginLeft: "60%"}}source={{uri: "https://www.pngall.com/wp-content/uploads/13/Red-Heart-PNG-Image-File.png"}}></Image>
+        </View> 
+        <Text style={styles.title}>{title}</Text>
+        <View style={styles.row}>
+          <Image style={{width: 12, height: 12}}source={{uri: "https://i.pinimg.com/originals/92/13/df/9213df2e7622e767f9fd09fcd656c5cb.jpg"}}></Image>
+          <Text style={styles.subtitle}>{speed}</Text>
+          <Image style={{width: 12, height: 12}} source={{uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN4fVwrk12XVeUx2BrHd8KMf3LUSYLsZ8WCtxjjHj3Ng&s"}}></Image>
+          <Text style={styles.subtitle}>{price}</Text>
+          <Image style={{width: 12, height: 12}} source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Question_mark.png/640px-Question_mark.png"}}></Image>
+          <Text style={styles.subtitle}>{level}</Text>
+        </View>
+    </ImageBackground>
+  </View>
+);
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -63,56 +60,71 @@ function App(): JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+      <FlatList
+      style={{
+        backgroundColor: "white"
+      }}
+        data={itemData}
+        renderItem={({item}) => <Item title={item.data.recipeName} image = {item.data.imageUrl} speed = {item.data.cookingTime} price= {item.data.amountOfIngredients} level={item.data.recipeDifficulty}/>}
+        keyExtractor={item => item.data.recipeName}
+        horizontal={false}
+        numColumns={2}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
-  sectionTitle: {
+  item: {
+    backgroundColor: '#f9c2ff',
+    marginVertical: 10,
+    marginHorizontal: 10,
+    width: "45%",
+    maxHeight: 175,
+    borderRadius: 10,
+  },
+  title: {
     fontSize: 24,
-    fontWeight: '600',
+    color: "white",
+    marginTop:"60%",
+    fontWeight: "bold",
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  subtitle: {
+    fontSize: 12,
+    color: "white"
   },
-  highlight: {
-    fontWeight: '700',
-  },
+  row: {
+    flexDirection: 'row',
+  }
 });
+const itemData = [
+  {
+    data: data.recipes[0]
+  },
+  {
+    data: data.recipes[1]
+  },
+  {
+    data: data.recipes[2]
+  },
+  {
+    data: data.recipes[3]
+  },
+  {
+    data: data.recipes[4]
+  },
+  {
+    data: data.recipes[5]
+  },
+  {
+    data: data.recipes[6]
+  },
+  {
+    data: data.recipes[7]
+  }
+]
 
 export default App;
